21897ece2a235a0de176f4d83939768e85a93abe
Javi en el Curso <ascendia.javier@gmail.com>
2018-07-10 20:04:26 +0200

    (DEV) Add jscript file

---
+++ js/jscript.js
@@ -0,0 +1,9 @@
+function body_onLoad() {
+	// Una vez que se haya cargado la página, se ejecuta este evento
+}
+
+function body_onUnLoad() {
+	// Una vez que se haya descargado la página, se ejecuta este evento
+}
+
+// console.log("");
